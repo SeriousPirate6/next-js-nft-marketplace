14cf810d2b328ba9ecea2e74d8b6e0a6c0422dbe
SeriousPirate6 <whatyoudontexpect3@gmail.com>
2022-07-20 01:03:03 +0200

    Added Header and updated _app.js and index.js.
    - Added sell-nft.js;
    - added networkMapping for frotend updating from backend.
    - added postcss and tailwind dependencies.
    
    [1:01:43:14]

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.SERVER_URL
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplce</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* In this way we connect to our Moralis DAPP */}
+            <MoralisProvider appId={APP_ID} servelUrl={SERVER_URL}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
