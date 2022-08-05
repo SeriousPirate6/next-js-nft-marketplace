5f3e5f226524e8bc760fe53c295a2b8db55c918f
SeriousPirate6 <whatyoudontexpect3@gmail.com>
2022-08-05 17:50:24 +0200

    Updated tokens and cloud function.

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -1,1 +1,10 @@
-console.log("Hi")
+// Create a new table called "ActiveItem"
+// Add items when they are listed on the marketplace
+// Remove then when they are bought or cancelled
+
+Moralis.Cloud.afterSave("ItemListed", async (request) => {
+    // Everytime event gets triggered twice, once on unconfirmed, again on confirmed
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info("Looking for confirmed Tx")
+})
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Header from "../components/Header"
 import Head from "next/head"
 
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID
-const SERVER_URL = process.env.SERVER_URL
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             {/* In this way we connect to our Moralis DAPP */}
-            <MoralisProvider appId={APP_ID} servelUrl={SERVER_URL}>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
                 <Header />
                 <Component {...pageProps} />
             </MoralisProvider>
